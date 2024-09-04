d501c6d7b9b630bd84ac37b8d90970ff12d54ea9
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2024-09-04 02:58:26 -0300

    chore: execute jobs from args

--- Program.cs
+++ Program.cs
@@ -11,23 +11,25 @@ public class Program
     private static readonly string Password = Environment.GetEnvironmentVariable("bluesky_password") ?? throw new ApplicationException("variable $bluesky_password not found");
     private static readonly string EmbbedSourceExtractorUrl = Environment.GetEnvironmentVariable("embbed_source_url") ?? throw new ApplicationException("variable $embbed_source_url not found");
     
-    private static readonly ILogger<ContentCreationWorker> Logger = LoggerFactory.Create(b =>
+    private static readonly ILogger<Program> Logger = LoggerFactory.Create(b =>
     {
         b.SetMinimumLevel(LogLevel.Debug).AddSimpleConsole();
-    }).CreateLogger<ContentCreationWorker>();
+    }).CreateLogger<Program>();
     
-    public static void Main() => MainAsync().Wait();
-    private static async Task MainAsync()
+    public static void Main(string[] args) => MainAsync(args).Wait();
+    private static async Task MainAsync(string[] args)
     {
-        
         Logger.LogInformation(@"Worker ""bsky.bot"" started at: {0}", DateTime.Now);
         using var dataRepository = new DataRepository();
         var blueSkyApi = new BlueSky(BlueSky, Email, Password, EmbbedSourceExtractorUrl);
         await blueSkyApi.Login();
         var interactionWorker = new InteractionWorker(blueSkyApi,  dataRepository, OllamaUrl);
         var contentCreationWorker = new ContentCreationWorker(blueSkyApi, dataRepository, OllamaUrl);
+        if (args.Any(a => a == "scr"))
+        {
+            await contentCreationWorker.ExecuteAsync();
+        }
         await interactionWorker.ExecuteAsync();
-        await contentCreationWorker.ExecuteAsync();
         Logger.LogInformation(@"Worker ""bsky.bot"" ended at: {0}", DateTime.Now);
     }
 }
